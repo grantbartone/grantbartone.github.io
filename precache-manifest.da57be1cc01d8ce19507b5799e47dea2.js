4fa6922eac6817fc621a8318c902fe277e860cca
Grant Bartone <grant@pared.com>
2020-10-02 08:37:49 -0700

    GameOfLife release

---
+++ precache-manifest.da57be1cc01d8ce19507b5799e47dea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeb6feb320459315b35c1c33eba10532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e910f10fa8b9dad653ba",
+    "url": "/static/css/main.bb32a861.chunk.css"
+  },
+  {
+    "revision": "4f4d2d570dcf61f1a9f5",
+    "url": "/static/js/2.23bb9138.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/static/js/2.23bb9138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e910f10fa8b9dad653ba",
+    "url": "/static/js/main.30588d7a.chunk.js"
+  },
+  {
+    "revision": "4632d8bebab62e0a1c37",
+    "url": "/static/js/runtime-main.5874b41f.js"
+  },
+  {
+    "revision": "beb6a98d8677c17efea2406432242caa",
+    "url": "/static/media/logo-icons.beb6a98d.svg"
+  }
+]);
